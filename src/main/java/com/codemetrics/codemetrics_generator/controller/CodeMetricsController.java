a70c63d4b9b903908a87725e323a75ac4bdcb59f
Yenifer Mora <ymoras@unal.edu.co>
2024-07-01 18:40:37 -0500

    config: cross origins

--- src/main/java/com/codemetrics/codemetrics_generator/controller/CodeMetricsController.java
+++ src/main/java/com/codemetrics/codemetrics_generator/controller/CodeMetricsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/codemetrics")
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class CodeMetricsController {
 
     private final CodeMetricsConfigProperties codeMetricsConfigProperties;
