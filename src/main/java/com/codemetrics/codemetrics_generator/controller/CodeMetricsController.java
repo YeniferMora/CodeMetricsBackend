d6e54a39da7b3919a5cd648669b8aa44af33889b
Yenifer Mora <ymoras@unal.edu.co>
2024-07-01 19:58:57 -0500

    config: cross origins vercel

--- src/main/java/com/codemetrics/codemetrics_generator/controller/CodeMetricsController.java
+++ src/main/java/com/codemetrics/codemetrics_generator/controller/CodeMetricsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/codemetrics")
-@CrossOrigin(origins = {"http://localhost:3000"})
+@CrossOrigin(origins = {"http://localhost:3000","https://codemetrics-virid.vercel.app"})
 public class CodeMetricsController {
 
     private final CodeMetricsConfigProperties codeMetricsConfigProperties;
